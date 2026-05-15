2e13f164e83ae504b13b43f6f893e60be4dcfb4c
Robert Baumgartner <r.baumgartner-1@tudelft.nl>
2026-05-15 12:09:18 +0200

    Fix GPU softmax

--- src/backend/module/activation_functions/cuda/activations.cu
+++ src/backend/module/activation_functions/cuda/activations.cu
@@ -71,6 +71,7 @@ namespace {
    */
   template<int maxoffset>
   __forceinline__ __device__ void warpMaxReduce(volatile ftype* const input, const tensorSize_t stride, const int offset) {
+    // TODO: warp shuffle for newer architectures
     if(maxoffset == 32) {
       if(offset + 32 < stride) input[offset] = max(input[offset], input[offset + 32]);
     }
@@ -96,6 +97,7 @@ namespace {
    */
   template<int maxoffset>
   __forceinline__ __device__ void warpSumReduce(volatile ftype* const input, const tensorSize_t stride, const int offset) {
+    // TODO: warp shuffle for newer architectures
     if(maxoffset == 32) {
       if(offset + 32 < stride) input[offset] += input[offset + 32];
     }
@@ -115,6 +117,22 @@ namespace {
       if(offset + 1 < stride) input[offset] += input[offset + 1];
     }
   }
+
+  /**
+   * @brief For the softmax implementations.
+   */
+  template<typename T>
+  __forceinline__ __device__ ftype stableExp(const ftype x, const ftype maxValue) {
+    if constexpr (std::is_same_v<T, float>) {
+      return expf(x - maxValue);
+    }
+    else if constexpr (std::is_same_v<T, double>) {
+      return exp(x - maxValue);
+    }
+    else {
+      static_assert(always_false<T>, "ftype encountered unexpected type");
+    }
+  } 
   
   /**
    * @brief Here we find the maximum within 'stride'. Assumption: One warp does exactly one element of stride!
@@ -158,20 +176,10 @@ namespace {
 
     const auto strideOffset = gid / stride;
     const auto maxValue = maxValues[strideOffset];
+    ftype expVal = stableExp<ftype>(input[gid], maxValue);
 
     int tid = threadIdx.x;
     extern __shared__ ftype smem[]; // can lead to bank conflicts iff std::is_same_v<T, double>
-
-    ftype expVal = 0;
-    if constexpr (std::is_same_v<T, float>) {
-      expVal = expf(input[gid] - maxValue);
-    }
-    else if constexpr (std::is_same_v<T, double>) {
-      expVal = exp(input[gid] - maxValue);
-    }
-    else {
-      static_assert(always_false<T>, "ftype encountered unexpected type");
-    }
     smem[tid] = expVal;
     __syncthreads();
 
@@ -185,81 +193,100 @@ namespace {
   /**
    * @brief Like findMaxKernelOneWarp. The difference now is that the input size can be much larger. stride is 
    * 64 < stride <= threadsPerBlock. res has the maximum values stored.
+   * 
+   * In this initial version we assume one kernel per stride, to make matters simple to understand.
    */
   __global__ void findMaxKernelOneBlock(ftype* const res, const ftype* const input, const tensorSize_t stride, const tensorSize_t size) {
-    assert(blockDim.x % stride == 0); // guarantees that stride not spread across two blocks 
+    assert_debug(blockDim.x / stride == 0, "Kernel built for one stride per block, blockDim.x is < stride"); 
 
-    int gid = blockIdx.x * blockDim.x + threadIdx.x;
-    if(gid >= size)
-      return;
+    const int tid = threadIdx.x;
+    const int gid = blockIdx.x * stride + tid;
 
-    int tid = threadIdx.x;
     extern __shared__ ftype smem[]; // can lead to bank conflicts iff std::is_same_v<T, double>
-    smem[tid] = input[gid];
+
+    const tensorSize_t maxIdx = tid + blockDim.x;
+    const bool doPadding = maxIdx >= stride;
+    if(doPadding) {
+      smem[tid] = input[gid];
+    }
+    else {
+      smem[tid] = max(input[gid], input[gid + blockDim.x]);
+    }
     __syncthreads();
 
-    const tensorSize_t baseOffset = gid % stride;
-    for(tensorSize_t offset = (stride + 1) / 2; offset > 32; offset = (offset + 1) / 2) {
-      // ceil-division over two -> at least one thread crosses boundary iff stride % 2 == 1
-      if(baseOffset + offset < stride) {
+    for(tensorSize_t offset = blockDim.x >> 1; offset > 32; offset >>= 1) {
+      if(tid < offset) {
         smem[tid] = max(smem[tid], smem[tid + offset]);
       }
       __syncthreads();
     }
 
-    volatile ftype* const start = smem + (tid / stride) * stride;
-    const int offset = gid % stride;
-    warpMaxReduce<32>(start, stride, offset);
+    // TODO: warp shuffle for newer architectures
+    volatile ftype* const start = smem;
+    if(tid < 32) start[tid] = max(start[tid], start[tid + 32]);
+    if(tid < 16) start[tid] = max(start[tid], start[tid + 16]);
+    if(tid < 8) start[tid] = max(start[tid], start[tid + 8]);
+    if(tid < 4) start[tid] = max(start[tid], start[tid + 4]);
+    if(tid < 2) start[tid] = max(start[tid], start[tid + 2]);
+    if(tid < 1) start[tid] = max(start[tid], start[tid + 1]);
 
-    if(baseOffset == 0) {
-      res[gid / stride] = start[0];
+    if(tid == 0) { // one block per stride
+      res[blockIdx.x] = start[0];
     }
   }
 
   /**
    * @brief Just like stableSoftmaxKernelOneWarp, but this one works across a whole block, not just a warp.
+   * 
+   * In this initial version we assume one kernel per stride, to make matters simple to understand.
    */
   template<typename T>
   __global__ void stableSoftmaxKernelOneBlock(ftype* res, const ftype* const input, const ftype* const maxValues,
-                                          const tensorSize_t stride, const tensorSize_t size) {
-    assert(blockDim.x % stride == 0); // guarantees that stride not spread across two blocks 
+                                              const tensorSize_t stride, const tensorSize_t size) {
+    assert_debug(blockDim.x / stride == 0, "Kernel built for one stride per block, blockDim.x is < stride"); 
 
-    int gid = blockIdx.x * blockDim.x + threadIdx.x;
-    if(gid >= size)
-      return;
+    const int tid = threadIdx.x;
+    const int gid = blockIdx.x * stride + tid;
 
-    const auto strideOffset = gid / stride;
-    const auto maxValue = maxValues[strideOffset];
+    const auto maxValue = maxValues[blockIdx.x]; // TODO: i can share this one within a warp?
 
-    extern __shared__ ftype smem[];
-    int tid = threadIdx.x;
+    extern __shared__ ftype smem[]; // can lead to bank conflicts iff std::is_same_v<T, double>
 
-    ftype expVal = 0;
-    if constexpr (std::is_same_v<T, float>) {
-      expVal = expf(input[gid] - maxValue);
-    }
-    else if constexpr (std::is_same_v<T, double>) {
-      expVal = exp(input[gid] - maxValue);
-    }
-    else {
-      static_assert(always_false<T>, "ftype encountered unexpected type");
-    }
+    ftype expVal = stableExp<ftype>(input[gid], maxValue);
     smem[tid] = expVal;
     __syncthreads();
 
-    const tensorSize_t baseOffset = gid % stride;
-    for(tensorSize_t offset = (stride + 1) / 2; offset > 32; offset = (offset + 1) / 2) {
-      if(baseOffset + offset < stride) {
-        smem[tid] = smem[tid] + smem[tid + offset]; // ceil-division -> last thread can cross boundary
+    const tensorSize_t maxIdx = tid + blockDim.x;
+    const bool doPadding = maxIdx >= stride;
+
+    ftype expValOffset = 0;
+    if(!doPadding) { // some threads will be idle here
+      expValOffset = stableExp<ftype>(input[gid + blockDim.x], maxValue);
+    }
+
+    smem[maxIdx] = expValOffset;
+    __syncthreads();
+
+    for(tensorSize_t offset = blockDim.x; offset > 32; offset >>= 1) {
+      if(tid < offset) {
+        smem[tid] = smem[tid] + smem[tid + offset];
       }
       __syncthreads();
     }
 
-    volatile ftype* const start = smem + (tid / stride) * stride;
-    const int offset = gid % stride;
-    warpSumReduce<32>(start, stride, offset);
+    // TODO: warp shuffle for newer architectures
+    volatile ftype* const start = smem;
+    if(tid < 32) start[tid] = start[tid] += start[tid + 32];
+    if(tid < 16) start[tid] = start[tid] += start[tid + 16];
+    if(tid < 8) start[tid] = start[tid] += start[tid + 8];
+    if(tid < 4) start[tid] = start[tid] += start[tid + 4];
+    if(tid < 2) start[tid] = start[tid] += start[tid + 2];
+    if(tid < 1) start[tid] = start[tid] += start[tid + 1];
 
     res[gid] = expVal / start[0];
+    if(!doPadding) {
+      res[gid + blockDim.x] = expValOffset / start[0];
+    }
   }
 }
 
@@ -353,21 +380,22 @@ namespace cuda_impl {
       cudaErrchk(cudaDeviceSynchronize());
     }
     else if (stride <= 256) {
-      // threads per block needs to be multiple of stride
       constexpr int maxThreadsPerBlock = 256;
       assert_debug(stride <= maxThreadsPerBlock, "If you adapt maxThreadsPerBlock you also have to adapt the limit in the if-clause above");
 
       const int nStrides = in.getSize() / stride;
-      const int stridesPerBlock = max(maxThreadsPerBlock / stride, 1);
+      constexpr int stridesPerBlock = 1; // if multiple strides per block allowed (adapt kernels!): max(maxThreadsPerBlock / stride, 1);
 
-      const int threadsPerBlock = stridesPerBlock * stride; 
-      const int blocks = (nStrides + stridesPerBlock - 1) / stridesPerBlock;
+      // threads per block needs to be multiple of stride, rounded toward the next multiple of 32
+      const int paddedStride = ((stride + 31) / 32) * 32;
+      const int threadsPerBlock = paddedStride * stridesPerBlock / 2; // over 2 for efficiency in reduction 
+      const int blocks = (nStrides + stridesPerBlock - 1) / stridesPerBlock; // gerneralized version iff multiple strides per block allowed
 
-      findMaxKernelOneBlock<<<blocks, threadsPerBlock, threadsPerBlock * sizeof(ftype)>>>(maxValues, in.getData(), stride, in.getSize());
+      findMaxKernelOneBlock<<<blocks, threadsPerBlock, 2 * threadsPerBlock * sizeof(ftype)>>>(maxValues, in.getData(), stride, in.getSize());
       cudaErrchk(cudaDeviceSynchronize());
 
-      stableSoftmaxKernelOneBlock<ftype><<<blocks, threadsPerBlock, threadsPerBlock * sizeof(ftype)>>>(res.getData(), in.getData(), 
-                                                                                                       maxValues, stride, in.getSize());
+      stableSoftmaxKernelOneBlock<ftype><<<blocks, threadsPerBlock, 2 * threadsPerBlock * sizeof(ftype)>>>(res.getData(), in.getData(), maxValues, 
+                                                                                                       stride, in.getSize());
       cudaErrchk(cudaDeviceSynchronize());
     }
     else {
