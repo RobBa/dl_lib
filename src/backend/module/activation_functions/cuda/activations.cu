7091379cae44322e11180754b7955929223c506d
Robert Baumgartner <r.baumgartner-1@tudelft.nl>
2026-05-15 16:23:58 +0200

    Fixed softmax for good

--- src/backend/module/activation_functions/cuda/activations.cu
+++ src/backend/module/activation_functions/cuda/activations.cu
@@ -223,12 +223,14 @@ namespace {
 
     // TODO: warp shuffle for newer architectures
     volatile ftype* const start = smem;
-    if(tid < 32) start[tid] = max(start[tid], start[tid + 32]);
-    if(tid < 16) start[tid] = max(start[tid], start[tid + 16]);
-    if(tid < 8) start[tid] = max(start[tid], start[tid + 8]);
-    if(tid < 4) start[tid] = max(start[tid], start[tid + 4]);
-    if(tid < 2) start[tid] = max(start[tid], start[tid + 2]);
-    if(tid < 1) start[tid] = max(start[tid], start[tid + 1]);
+    if(tid < 32) {
+      start[tid] = max(start[tid], start[tid + 32]);
+      start[tid] = max(start[tid], start[tid + 16]);
+      start[tid] = max(start[tid], start[tid + 8]);
+      start[tid] = max(start[tid], start[tid + 4]);
+      start[tid] = max(start[tid], start[tid + 2]);
+      start[tid] = max(start[tid], start[tid + 1]);
+    }
 
     if(tid == 0) { // one block per stride
       res[blockIdx.x] = start[0];
@@ -248,13 +250,10 @@ namespace {
     const int tid = threadIdx.x;
     const int gid = blockIdx.x * stride + tid;
 
-    const auto maxValue = maxValues[blockIdx.x]; // TODO: i can share this one within a warp?
-
     extern __shared__ ftype smem[]; // can lead to bank conflicts iff std::is_same_v<T, double>
-
+    const auto maxValue = maxValues[blockIdx.x];
     ftype expVal = stableExp<ftype>(input[gid], maxValue);
     smem[tid] = expVal;
-    __syncthreads();
 
     const tensorSize_t maxIdx = tid + blockDim.x;
     const bool doPadding = maxIdx >= stride;
@@ -269,19 +268,22 @@ namespace {
 
     for(tensorSize_t offset = blockDim.x; offset > 32; offset >>= 1) {
       if(tid < offset) {
-        smem[tid] = smem[tid] + smem[tid + offset];
+        smem[tid] += smem[tid + offset];
       }
       __syncthreads();
     }
 
     // TODO: warp shuffle for newer architectures
     volatile ftype* const start = smem;
-    if(tid < 32) start[tid] = start[tid] += start[tid + 32];
-    if(tid < 16) start[tid] = start[tid] += start[tid + 16];
-    if(tid < 8) start[tid] = start[tid] += start[tid + 8];
-    if(tid < 4) start[tid] = start[tid] += start[tid + 4];
-    if(tid < 2) start[tid] = start[tid] += start[tid + 2];
-    if(tid < 1) start[tid] = start[tid] += start[tid + 1];
+    if(tid < 32) {
+      start[tid] += start[tid + 32];
+      start[tid] += start[tid + 16];
+      start[tid] += start[tid + 8];
+      start[tid] += start[tid + 4];
+      start[tid] += start[tid + 2];
+      start[tid] += start[tid + 1];
+    }
+    __syncthreads();
 
     res[gid] = expVal / start[0];
     if(!doPadding) {
@@ -379,16 +381,18 @@ namespace cuda_impl {
       }
       cudaErrchk(cudaDeviceSynchronize());
     }
-    else if (stride <= 256) {
+    else if (stride <= 512) {
       constexpr int maxThreadsPerBlock = 256;
-      assert_debug(stride <= maxThreadsPerBlock, "If you adapt maxThreadsPerBlock you also have to adapt the limit in the if-clause above");
+      assert_debug(stride <= 2 * maxThreadsPerBlock, "If you adapt maxThreadsPerBlock you also have to adapt the limit in the if-clause above");
 
       const int nStrides = in.getSize() / stride;
       constexpr int stridesPerBlock = 1; // if multiple strides per block allowed (adapt kernels!): max(maxThreadsPerBlock / stride, 1);
 
-      // threads per block needs to be multiple of stride, rounded toward the next multiple of 32
-      const int paddedStride = ((stride + 31) / 32) * 32;
-      const int threadsPerBlock = paddedStride * stridesPerBlock / 2; // over 2 for efficiency in reduction 
+      // threads per block needs to be power of 2 for reduction to resolve cleanly
+      int paddedStride = 1;
+      while(paddedStride < stride) paddedStride <<= 1;
+      int threadsPerBlock = paddedStride / 2;
+
       const int blocks = (nStrides + stridesPerBlock - 1) / stridesPerBlock; // gerneralized version iff multiple strides per block allowed
 
       findMaxKernelOneBlock<<<blocks, threadsPerBlock, 2 * threadsPerBlock * sizeof(ftype)>>>(maxValues, in.getData(), stride, in.getSize());
